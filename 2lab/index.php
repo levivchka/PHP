5a377067eeb3d375c4c7c691cf2fa2e94e84ab86
levivchka <vikanikottin@gmail.com>
2025-02-25 11:56:38 +0300

    first commit

---
+++ 2lab/index.php
@@ -0,0 +1,26 @@
+<?php 
+    $a = 5;
+    $_m = 'f';
+    $D = 9;
+
+    $a = 2;
+    $b = '2';
+
+    echo  ($a + $b);
+
+    $f = (string)$D;
+    $m =(int)$_m;
+    //echo gettype($f);
+    // print_r($D);
+    // var_dump($m);       
+     
+     define("pi", 3.148738473);
+     //echo pi;
+
+     $c = 3.7;
+     $e = 3.4;
+
+     echo "c = $c<BR>";
+     echo "e = $e<BR>";
+     echo 'floor: $c = '.floor($c).', $e = '.floor($e).'<BR>';
+     
